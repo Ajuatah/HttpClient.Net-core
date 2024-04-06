079af6aff163d0503032233e1916519fdebd486c
Ajuatah <ajuatahcodingarena@gmail.com>
2024-04-06 21:36:08 +0100

    added more endpoints

--- HttpClient.Net core/Controllers/PostController.cs
+++ HttpClient.Net core/Controllers/PostController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using System.Text;
 using System.Net.Http;
+using System.Net;
 
 namespace HttpClient.Net_core.Controllers
 {
@@ -12,6 +13,7 @@ namespace HttpClient.Net_core.Controllers
     public class PostController : ControllerBase
     {
         private readonly System.Net.Http.HttpClient _httpClient;
+
 
         public PostController(System.Net.Http.HttpClient httpClient)
         {
@@ -118,6 +120,134 @@ namespace HttpClient.Net_core.Controllers
                 return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
             }
         }
+
+        [HttpGet("Authorization")]
+        public async Task<IActionResult> Authorization()
+        {
+            var guid = Guid.NewGuid();
+            var baseUrl = "https://jsonplaceholder.typicode.com/";
+            var url = $"{baseUrl}posts/{guid}";
+
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer Your_Token_Here");
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        [HttpGet("Timeout")]
+        public async Task<IActionResult> Timeout()
+        {
+            var guid = Guid.NewGuid();
+            var baseUrl = "https://jsonplaceholder.typicode.com/";
+            var url = $"{baseUrl}posts/{guid}";
+
+            _httpClient.DefaultRequestHeaders.Add("Authorization", "Bearer Your_Token_Here");
+            _httpClient.Timeout = TimeSpan.FromSeconds(30);
+
+            var response = await _httpClient.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+        }
+
+        [HttpGet("DownloadImage")]
+        public async Task<IActionResult> DownloadImage()
+        {
+            var url = "https://via.placeholder.com/150";
+
+            var response = await _httpClient.GetByteArrayAsync(url);
+
+            if (response != null && response.Length > 0)
+            {
+                return File(response, "image/jpeg", "image.jpg");
+            }
+            else
+            {
+                return BadRequest("Failed to download image.");
+            }
+        }
+
+        [HttpGet("OneClientWithMultipleRequests")]
+        public async Task<IActionResult> OneClientWithMultipleRequests()
+        {
+            var guids = new List<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+            foreach (var guid in guids)
+            {
+                var baseUrl = "https://jsonplaceholder.typicode.com/";
+                var url = $"{baseUrl}posts/{guid}";
+
+                var response = await _httpClient.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var data = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(data);
+                }
+                else
+                {
+                    Console.WriteLine(response.StatusCode.ToString());
+                    Console.WriteLine(await response.Content.ReadAsStringAsync());
+                }
+            }
+
+            return Ok("Multiple requests completed successfully.");
+        }
+
+
+        [HttpGet("MakingUseofProxies")]
+        public async Task<IActionResult> MakingUseofProxies()
+        {
+            // Define proxy settings
+            HttpClientHandler handler = new HttpClientHandler
+            {
+                // Example of a public proxy URL and port
+                Proxy = new WebProxy(new Uri("http://123.456.789.123:8080")),
+                UseProxy = true
+            };
+
+            
+           
+            var guid = Guid.NewGuid();
+
+            
+            var baseUrl = "https://jsonplaceholder.typicode.com/";
+
+            
+            var url = $"{baseUrl}posts/{guid}";
+
+            var response = await _httpClient.GetAsync(url);
+
+          
+            if (response.IsSuccessStatusCode)
+            {
+                var data = await response.Content.ReadAsStringAsync();
+                return Ok(data);
+            }
+            else
+            {
+                return StatusCode((int)response.StatusCode, await response.Content.ReadAsStringAsync());
+            }
+        }
+
+
+
     }
 
     public class Post
